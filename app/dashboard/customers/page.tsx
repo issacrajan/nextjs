38d6d4ad6dbdc42aee08181f60c8d487ceeeaf66
issac <issac.rajan@gmail.com>
2024-01-20 12:32:58 +0530

    init

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CustomersPage = () => {
+  return <p>customer page</p>;
+};
+
+export default CustomersPage;
---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <PrimeReactProvider>
+      <html lang="en">
+        <body className={`${inter.className} antialiased`}>{children}</body>
+      </html>
+    </PrimeReactProvider>
+  );
+}
